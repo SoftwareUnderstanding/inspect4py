bbef307cd1374d7de0106b43bfa808bdfb68d4d5
Daniel Garijo <dgarijov@gmail.com>
2021-06-10 22:27:05 +0200

    Added annotations and improved benchmarking options

--- test_directory_type.py
+++ test_directory_type.py
@@ -12,12 +12,27 @@ benchmark_path = "main_command/execution_commands_python_benchmark.csv"
 
 benchmark_df = pd.read_csv(benchmark_path)
 
+if not os.path.isdir(repo_path):
+    os.makedirs(repo_path)
+
+# Download repos if they don't exist in the repo_path
+for index, row in benchmark_df.iterrows():
+    current_repo = "https://github.com/" + row["repository"]
+    print("Downloading: " + row["repository"])
+    cmd = 'cd ' + repo_path + ' && git clone ' + current_repo
+    proc = subprocess.Popen(cmd.encode('utf-8'), shell=True, stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    # print(stderr)
+
+
+# Process
 num_correct = 0
 num_total = benchmark_df.shape[0] # rows
 details = {}
-for dirname in os.listdir(repo_path):
-    print("processing: " +dirname)
-    cmd = 'python code_inspector.py -i '+ repo_path + dirname
+for dir_name in os.listdir(repo_path):
+    print("processing: " + dir_name)
+    cmd = 'python code_inspector.py -i ' + repo_path + dir_name
     proc = subprocess.Popen(cmd.encode('utf-8'), shell=True, stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
@@ -27,7 +42,7 @@ for dirname in os.listdir(repo_path):
     file.close()
     current_type = "unknown"
     # This will have to be changed if dir_type JSON definition is changed
-    if data['dir_type']:
+    if 'dir_type' in data.keys() and data['dir_type']:
         # print(dirname+" "+str(data['dir_type']))
         print(str(data['dir_type']))
         try:
@@ -40,10 +55,10 @@ for dirname in os.listdir(repo_path):
                 current_type = data['dir_type']
 
     for index,row in benchmark_df.iterrows():
-        if dirname in row["repository"]:
+        if dir_name in row["repository"]:
 
             if row["type"] in current_type:
                 num_correct += 1
-                print("#### CORRECT type for " + dirname)
+                print("#### CORRECT type for " + dir_name)
 
 print("Accuracy: " + str(num_correct) + " out of " + str(num_total) + ". "+ str(num_correct/num_total))
