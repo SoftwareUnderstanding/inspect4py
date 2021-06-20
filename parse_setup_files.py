2de723c42c3b64bd5c24c556ab1ef900e28da195
Daniel Garijo <dgarijov@gmail.com>
2021-06-20 18:45:48 +0200

    Transformed return types into lists

--- parse_setup_files.py
+++ parse_setup_files.py
@@ -7,131 +7,132 @@ from unittest import mock
 import configparser
 import setuptools
 
+
 def parse_setup_py(parent_dir):
-    setup_info={}    
+    setup_info = {}
     setup_content = open(os.path.join(parent_dir, "setup.py")).read().split("\n")
-    console_index=0
-    flag_console=0
-    name_index=0
-    flag_name=0
+    console_index = 0
+    flag_console = 0
+    name_index = 0
+    flag_name = 0
     name = "UNKNOWN"
     for elem in setup_content:
         if 'name=' in elem:
-            flag_name=1
+            flag_name = 1
             break
         else:
-            name_index+=1
+            name_index += 1
     if flag_name:
-        name=setup_content[name_index].split('=')[1].split(',')[0].replace('\'', '')
+        name = setup_content[name_index].split('=')[1].split(',')[0].replace('\'', '')
         for elem in setup_content:
             if 'console_scripts' in elem:
-                 flag_console=1
-                 break
+                flag_console = 1
+                break
             else:
-                 console_index += 1
+                console_index += 1
         if flag_console:
-            cs_list=[]
+            cs_list = []
             setup_info["run"] = []
-            for elem in setup_content[console_index+1:]:
+            for elem in setup_content[console_index + 1:]:
                 if ']' not in elem:
-                    cs_string=elem.split("=")[0].strip().replace('\'', '')
+                    cs_string = elem.split("=")[0].strip().replace('\'', '')
                     cs_list.append(cs_string)
-                    setup_info["run"].append(cs_string+ " --help")
+                    setup_info["run"].append(cs_string + " --help")
                 else:
                     break
-            if name in cs_list:     
-                setup_info["type"] = "package"
+            if name in cs_list:
+                setup_info["type"] = ["package"]
                 setup_info["installation"] = "pip install " + name
             else:
-                setup_info["type"] = "library and package"
+                setup_info["type"] = ["library","package"]
                 setup_info["installation"] = "pip install " + name
                 setup_info["run"].append("import " + name)
             return setup_info
         else:
-            setup_info["type"] = "library"
+            setup_info["type"] = ["library"]
             setup_info["installation"] = "pip install " + name
             setup_info["run"] = "import " + name
             return setup_info
     else:
-        setup_info["type"] = "library"
+        setup_info["type"] = ["library"]
         setup_info["installation"] = "pip install " + name
         setup_info["run"] = "import " + name
         return setup_info
- 
- 
 
-def inspect_setup_cfg(parent_dir , name, error=2):
-     setup_info={}
-     setup_cfg= os.path.join(parent_dir, "setup.cfg")
 
-     # checking if we have setup.cfg 
-     if Path(setup_cfg).is_file():
-         parser = configparser.ConfigParser()
-         with open("setup.cfg", "r") as f:
-             parser.read_file(f)
-         #extracting the name
-         if not name: 
-             section="metadata"
-             if section in parser:
-                 metadata=dict(parser["metadata"])
-                 if "name" in metadata:
-                    name=metadata["name"]
-                 else:
+def inspect_setup_cfg(parent_dir, name, error=2):
+    setup_info = {}
+    setup_cfg = os.path.join(parent_dir, "setup.cfg")
+
+    # checking if we have setup.cfg
+    if Path(setup_cfg).is_file():
+        parser = configparser.ConfigParser()
+        with open("setup.cfg", "r") as f:
+            parser.read_file(f)
+        # extracting the name
+        if not name:
+            section = "metadata"
+            if section in parser:
+                metadata = dict(parser["metadata"])
+                if "name" in metadata:
+                    name = metadata["name"]
+                else:
                     try:
                         name = subprocess.getoutput("python setup.py --name")
                     except:
                         name = "UNKNOWN"
-             else:
-                 try:
-                     name = subprocess.getoutput("python setup.py --name")
-                 except:
-                     name = "UNKNOWN"
-         #extracting entry_points
-         section="entry_points"
-         for s in parser:
-             if section in s:
-                 data=dict(parser[s])
-                 if "console_scripts" in data:
-                     console_scripts=data["console_scripts"].splitlines()
-                     console_scripts.remove('')
-                     setup_info["run"] = []
-                     cs_list=[] 
-                     for cs in console_scripts:
-                         cs_string=cs.split("=")[0].rstrip() 
-                         setup_info["run"].append(cs_string+ ' --help')
-                         cs_list.append(cs_string)
-                     if  name not in cs_list:
-                         setup_info["type"] = "library and package"
-                         setup_info["run"].append("import " + name)
-                     else: 
-                         setup_info["type"] = "package"
-                     return setup_info
-         if error!=3:
-             setup_info["type"] = "library"
-             setup_info["installation"] = "pip install " + name
-             setup_info["run"] = "import " + name
-             return setup_info
-  
-         return setup_info
-     else:
-         # This is the last resource. We got here because an exception
-         # or becasue we are not able to open setup.py and there is not setup.cfg
-         # Classify it as package
-         try:
-             if not name:
-                 name = subprocess.getoutput("python setup.py --name")
-         except:
-                 name = "UNKNOWN"
-         if error == 1:
-             setup_info["type"] = "package"
-             setup_info["installation"] = "pip install " + name
-             setup_info["run"] =  name + " --help"
+            else:
+                try:
+                    name = subprocess.getoutput("python setup.py --name")
+                except:
+                    name = "UNKNOWN"
+        # extracting entry_points
+        section = "entry_points"
+        for s in parser:
+            if section in s:
+                data = dict(parser[s])
+                if "console_scripts" in data:
+                    console_scripts = data["console_scripts"].splitlines()
+                    console_scripts.remove('')
+                    setup_info["run"] = []
+                    cs_list = []
+                    for cs in console_scripts:
+                        cs_string = cs.split("=")[0].rstrip()
+                        setup_info["run"].append(cs_string + ' --help')
+                        cs_list.append(cs_string)
+                    if name not in cs_list:
+                        setup_info["type"] = ["library", "package"]
+                        setup_info["run"].append("import " + name)
+                    else:
+                        setup_info["type"] = ["package"]
+                    return setup_info
+        if error != 3:
+            setup_info["type"] = ["library"]
+            setup_info["installation"] = "pip install " + name
+            setup_info["run"] = "import " + name
+            return setup_info
 
-         elif error == 2:
-             setup_info["type"] = "library"
-             setup_info["installation"] = "pip install " + name
-             setup_info["run"] = "import " + name
-         return setup_info
+        return setup_info
+    else:
+        # This is the last resource. We got here because an exception
+        # or becasue we are not able to open setup.py and there is not setup.cfg
+        # Classify it as package
+        try:
+            if not name:
+                name = subprocess.getoutput("python setup.py --name")
+        except:
+            name = "UNKNOWN"
+        if error == 1:
+            setup_info["type"] = ["package"]
+            setup_info["installation"] = "pip install " + name
+            setup_info["run"] = name + " --help"
+
+        elif error == 2:
+            setup_info["type"] = ["library"]
+            setup_info["installation"] = "pip install " + name
+            setup_info["run"] = "import " + name
+        return setup_info
+
 
 def inspect_setup(parent_dir):
     setup_info = {}
@@ -148,10 +149,10 @@ def inspect_setup(parent_dir):
                 module_name = os.path.basename(temp_fh.name).split(".")[0]
                 __import__(module_name)
         except:
-            print("extoy en el except")
-            name=""
-            error=1
-            setup_info=inspect_setup_cfg(abs_parent_dir, name, error)
+            # print("extoy en el except")
+            name = ""
+            error = 1
+            setup_info = inspect_setup_cfg(abs_parent_dir, name, error)
             os.chdir(current_dir)
             return setup_info
         finally:
@@ -164,55 +165,53 @@ def inspect_setup(parent_dir):
         # succesfully imported mock_setup 
         if mock_setup.call_args_list:
             # we have call_args to inspect.
-            print("moc_setup.call_args_list funciona %s" %mock_setup.call_args_list)
+            print("moc_setup.call_args_list works %s" % mock_setup.call_args_list)
             args, kwargs = mock_setup.call_args
             name = kwargs.get('name').lower()
-            entry_point= kwargs.get('entry_points')
+            entry_point = kwargs.get('entry_points')
             if not entry_point:
-                error=2
-                setup_info=inspect_setup_cfg(abs_parent_dir, name, error)
+                error = 2
+                setup_info = inspect_setup_cfg(abs_parent_dir, name, error)
                 os.chdir(current_dir)
                 return setup_info
             else:
                 os.chdir(current_dir)
                 if 'console_scripts' in entry_point:
                     setup_info["run"] = []
-                    cs_list=[]
+                    cs_list = []
                     for cs in entry_point['console_scripts']:
-                        cs_string=cs.split("=")[0].rstrip()
-                        setup_info["run"].append(cs_string+ " --help")
+                        cs_string = cs.split("=")[0].rstrip()
+                        setup_info["run"].append(cs_string + " --help")
                         cs_list.append(cs_string)
                     if name not in cs_list:
-                        setup_info["type"] = "library and package"
+                        setup_info["type"] = ["library", "package"]
                         setup_info["run"].append("import " + name)
-                    else: 
-                        setup_info["type"] = "package"
+                    else:
+                        setup_info["type"] = ["package"]
                     setup_info["installation"] = "pip install " + name
                     return setup_info
 
                 else:
-                    setup_info["type"] = "library"
+                    setup_info["type"] = ["library"]
                     setup_info["installation"] = "pip install " + name
                     setup_info["run"] = "import " + name
                     return setup_info
 
         else:
             # got an error with mock - lets check setup.cfg 
-            setup_cfg= os.path.join(abs_parent_dir, "setup.cfg")
+            setup_cfg = os.path.join(abs_parent_dir, "setup.cfg")
             if Path(setup_cfg).is_file():
-                name=""
-                error=3
-                setup_info=inspect_setup_cfg(abs_parent_dir, name, error)
+                name = ""
+                error = 3
+                setup_info = inspect_setup_cfg(abs_parent_dir, name, error)
                 if setup_info:
                     os.chdir(current_dir)
                     return setup_info
                 else:
-                    setup_info= parse_setup_py(abs_parent_dir)
+                    setup_info = parse_setup_py(abs_parent_dir)
                     os.chdir(current_dir)
                     return setup_info
             else:
-                setup_info= parse_setup_py(abs_parent_dir)
+                setup_info = parse_setup_py(abs_parent_dir)
                 os.chdir(current_dir)
                 return setup_info
-
-
