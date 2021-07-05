09a6474385c1972f0f219a91522840a2c5ea7ec4
rfilguei <rosa.filgueira@ed.ac.uk>
2021-07-05 17:22:42 +0100

    new test for multiple mains - indirect relation

---
+++ test/test_multiple_mains_indirect/foo.py
@@ -0,0 +1,6 @@
+# foo.py
+import bar
+
+def test():
+  print("I am in foo.py - going to call bar.test")
+  bar.test()
---
+++ test/test_multiple_mains_indirect/test.py
@@ -0,0 +1,13 @@
+# test.py
+import bar  # use an absolute import
+import foo
+
+def test():
+  print("I am in test.py")
+  foo.test()
+  def pp():
+      print("pp")
+  pp()
+
+if __name__ == '__main__':
+  test()
