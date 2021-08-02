4f244c8a69cda2b7554f411977b188714a841618
Daniel Garijo <dgarijov@gmail.com>
2021-08-02 18:11:05 +0200

    fix #13

--- test/test_code_inspector.py
+++ test/test_code_inspector.py
@@ -7,7 +7,7 @@ class Test(unittest.TestCase):
         json = [{"type": "service",
                  "run": "python /GitHub/test_repos/Chowlk/app.py"},
                 {"type": "script with main",
-                 "run": "python //GitHub/test_repos/Chowlk/converter.py"}]
+                 "run": "python /GitHub/test_repos/Chowlk/converter.py"}]
         type_script = extract_software_type(json)
         assert (type_script == "service")
 
