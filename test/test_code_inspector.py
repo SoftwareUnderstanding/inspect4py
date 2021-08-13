77e8fc4c120d8bff2ea4a7eaa799dd0eba1b4e22
rfilguei <r.filgueira@epcc.ed.ac.uk>
2021-08-13 11:04:14 +0100

    added two tests for call list

--- test/test_code_inspector.py
+++ test/test_code_inspector.py
@@ -1,5 +1,7 @@
 import unittest
+import shutil
 from code_inspector.utils import *
+from code_inspector.cli import *
 
 
 class Test(unittest.TestCase):
@@ -15,6 +17,31 @@ class Test(unittest.TestCase):
         json = []
         type_script = extract_software_type(json)
         assert (type_script == "not found")
+
+    def test_call_list_super(self):
+        dictionary = {'Rectangle': {}, 'Square': {'__init__': {'local': ['super_test.Rectangle.__init__']}}}
+        input_path="./test_files/test_inheritance/super_test.py"
+        output_dir="./output_dir"
+        control_flow= False
+        fig= False
+        cf_dir, json_dir = create_output_dirs(output_dir, control_flow)
+        code_info = CodeInspection(input_path, cf_dir, json_dir, fig, control_flow)
+        call_list_data = call_list_file(code_info)
+        shutil.rmtree(output_dir) 
+        assert (call_list_data['Rectangle'] == dictionary['Rectangle'])
+        assert (call_list_data['Square'] == dictionary['Square'])
+
+    def test_call_list_nested(self):
+        dictionary = {'functions': {'test': {'local': ['nested_call.MyClass.__init__', 'nested_call.MyClass.func']}}, 'body': {'local': ['test']}, 'MyClass': {'func': {'local': ['nested_call.MyClass.func.nested', 'print'], 'nested': {'nested': {'local': ['print']}}}}}
+        input_path="./test_files/test_inheritance/nested_call.py"
+        output_dir="./output_dir"
+        control_flow= False
+        fig= False
+        cf_dir, json_dir = create_output_dirs(output_dir, control_flow)
+        code_info = CodeInspection(input_path, cf_dir, json_dir, fig, control_flow)
+        call_list_data = call_list_file(code_info)
+        shutil.rmtree(output_dir) 
+        assert (call_list_data == dictionary)
 
 
 if __name__ == '__main__':
