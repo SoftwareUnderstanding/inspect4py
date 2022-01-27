8cbd1b81a01be9b4084887bdf753c9f494f88742
Daniel Garijo <dgarijov@gmail.com>
2022-01-27 19:50:13 +0100

    Cleaning non relevant files

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 from distutils.core import setup
 from setuptools import find_packages  # type: ignore
 
-from inspect4py import __version__
-
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -22,14 +20,17 @@ with open('requirements.txt', 'r') as f:
 
 packages = find_packages()
 
+version = {}
+with open("inspect4py/__init__.py") as fp:
+    exec(fp.read(), version)
+
 setup(
     name='inspect4py',
-    version=__version__,
+    version=version["__version__"],
     packages=packages,
     url='https://github.com/SoftwareUnderstanding/inspect4py',
     license='BSD-3-Clause',
     author='Rosa Filgueira and Daniel Garijo',
-    author_email='daniel.garijo@upm.es',
     description='Static code analysis package for Python repositories',
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
