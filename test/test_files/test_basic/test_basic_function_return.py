9939c1baf218dde7203f87394adee5e720f6a9a5
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2022-10-12 08:08:24 -0600

    improving the test

--- test/test_files/test_basic/test_basic_function_return.py
+++ test/test_files/test_basic/test_basic_function_return.py
@@ -1,3 +1,7 @@
+import sys
+
+variable_a=1
+
 def foo(arg1, arg2):
     print("Hello %s", arg1)
     return arg2
@@ -9,3 +13,15 @@ def foo2(arg1, arg2):
 def foo3(arg1, arg2):
     print("Hello %s", arg1)
     return str(arg2)
+
+def foo4(arg1, arg2):
+    a=[1,2,3]
+    return a
+
+def foo5(arg1, arg2):
+    a={}
+    a['name']= arg1
+    return a
+
+def foo6(arg1, arg2):
+    return [0,2,3]
