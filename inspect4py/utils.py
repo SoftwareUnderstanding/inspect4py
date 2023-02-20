b3f1766cbc386d1b6932e3253fffd4c2e65e26d1
Clark Wang <107419732+OEG-Clark@users.noreply.github.com>
2023-02-20 14:56:12 +0100

    Add functions for data flow and code tokens

--- inspect4py/utils.py
+++ inspect4py/utils.py
@@ -773,7 +773,6 @@ def get_github_metadata(input_path: str) -> dict:
 
     return github_metadata
 
-
 def find_index_init(depInfo, calls, class_init):
     index_remove=[]
     for dep in depInfo:
@@ -793,9 +792,261 @@ def update_list_calls(info, index_remove):
         if i in index_remove:
             continue
         updated_calls.append(info["calls"][i])
-    ### These lines are for removing duplicate calls 
+    ### These lines are for removing duplicate calls
     res = []
     for i in updated_calls :
         if i not in res:
             res.append(i)
     return res
+
+def tree_to_variable_index(root_node, index_to_code):
+    if (len(root_node.children) == 0 or root_node.type == 'string') and root_node.type != 'comment':
+        index = (root_node.start_point, root_node.end_point)
+        _, code = index_to_code[index]
+        if root_node.type != code:
+            return [(root_node.start_point, root_node.end_point)]
+        else:
+            return []
+    else:
+        code_tokens = []
+        for child in root_node.children:
+            code_tokens += tree_to_variable_index(child, index_to_code)
+        return code_tokens
+
+def DFG_python(root_node, index_to_code, states):
+    assignment = ['assignment', 'augmented_assignment', 'for_in_clause']
+    if_statement = ['if_statement']
+    for_statement = ['for_statement']
+    while_statement = ['while_statement']
+    do_first_statement = ['for_in_clause']
+    def_statement = ['default_parameter']
+    states = states.copy()
+    if (len(root_node.children) == 0 or root_node.type == 'string') and root_node.type != 'comment':
+        idx, code = index_to_code[(root_node.start_point, root_node.end_point)]
+        if root_node.type == code:
+            return [], states
+        elif code in states:
+            return [(code, idx, 'comesFrom', [code], states[code].copy())], states
+        else:
+            if root_node.type == 'identifier':
+                states[code] = [idx]
+            return [(code, idx, 'comesFrom', [], [])], states
+    elif root_node.type in def_statement:
+        name = root_node.child_by_field_name('name')
+        value = root_node.child_by_field_name('value')
+        DFG = []
+        if value is None:
+            indexs = tree_to_variable_index(name, index_to_code)
+            for index in indexs:
+                idx, code = index_to_code[index]
+                DFG.append((code, idx, 'comesFrom', [], []))
+                states[code] = [idx]
+            return sorted(DFG, key=lambda x: x[1]), states
+        else:
+            name_indexs = tree_to_variable_index(name, index_to_code)
+            value_indexs = tree_to_variable_index(value, index_to_code)
+            temp, states = DFG_python(value, index_to_code, states)
+            DFG += temp
+            for index1 in name_indexs:
+                idx1, code1 = index_to_code[index1]
+                for index2 in value_indexs:
+                    idx2, code2 = index_to_code[index2]
+                    DFG.append((code1, idx1, 'comesFrom', [code2], [idx2]))
+                states[code1] = [idx1]
+            return sorted(DFG, key=lambda x: x[1]), states
+    elif root_node.type in assignment:
+        if root_node.type == 'for_in_clause':
+            right_nodes = [root_node.children[-1]]
+            left_nodes = [root_node.child_by_field_name('left')]
+        else:
+            if root_node.child_by_field_name('right') is None:
+                return [], states
+            left_nodes = [x for x in root_node.child_by_field_name('left').children if x.type != ',']
+            right_nodes = [x for x in root_node.child_by_field_name('right').children if x.type != ',']
+            if len(right_nodes) != len(left_nodes):
+                left_nodes = [root_node.child_by_field_name('left')]
+                right_nodes = [root_node.child_by_field_name('right')]
+            if len(left_nodes) == 0:
+                left_nodes = [root_node.child_by_field_name('left')]
+            if len(right_nodes) == 0:
+                right_nodes = [root_node.child_by_field_name('right')]
+        DFG = []
+        for node in right_nodes:
+            temp, states = DFG_python(node, index_to_code, states)
+            DFG += temp
+
+        for left_node, right_node in zip(left_nodes, right_nodes):
+            left_tokens_index = tree_to_variable_index(left_node, index_to_code)
+            right_tokens_index = tree_to_variable_index(right_node, index_to_code)
+            temp = []
+            for token1_index in left_tokens_index:
+                idx1, code1 = index_to_code[token1_index]
+                temp.append((code1, idx1, 'computedFrom', [index_to_code[x][1] for x in right_tokens_index],
+                             [index_to_code[x][0] for x in right_tokens_index]))
+                states[code1] = [idx1]
+            DFG += temp
+        return sorted(DFG, key=lambda x: x[1]), states
+    elif root_node.type in if_statement:
+        DFG = []
+        current_states = states.copy()
+        others_states = []
+        tag = False
+        if 'else' in root_node.type:
+            tag = True
+        for child in root_node.children:
+            if 'else' in child.type:
+                tag = True
+            if child.type not in ['elif_clause', 'else_clause']:
+                temp, current_states = DFG_python(child, index_to_code, current_states)
+                DFG += temp
+            else:
+                temp, new_states = DFG_python(child, index_to_code, states)
+                DFG += temp
+                others_states.append(new_states)
+        others_states.append(current_states)
+        if tag is False:
+            others_states.append(states)
+        new_states = {}
+        for dic in others_states:
+            for key in dic:
+                if key not in new_states:
+                    new_states[key] = dic[key].copy()
+                else:
+                    new_states[key] += dic[key]
+        for key in new_states:
+            new_states[key] = sorted(list(set(new_states[key])))
+        return sorted(DFG, key=lambda x: x[1]), new_states
+    elif root_node.type in for_statement:
+        DFG = []
+        for i in range(2):
+            right_nodes = [x for x in root_node.child_by_field_name('right').children if x.type != ',']
+            left_nodes = [x for x in root_node.child_by_field_name('left').children if x.type != ',']
+            if len(right_nodes) != len(left_nodes):
+                left_nodes = [root_node.child_by_field_name('left')]
+                right_nodes = [root_node.child_by_field_name('right')]
+            if len(left_nodes) == 0:
+                left_nodes = [root_node.child_by_field_name('left')]
+            if len(right_nodes) == 0:
+                right_nodes = [root_node.child_by_field_name('right')]
+            for node in right_nodes:
+                temp, states = DFG_python(node, index_to_code, states)
+                DFG += temp
+            for left_node, right_node in zip(left_nodes, right_nodes):
+                left_tokens_index = tree_to_variable_index(left_node, index_to_code)
+                right_tokens_index = tree_to_variable_index(right_node, index_to_code)
+                temp = []
+                for token1_index in left_tokens_index:
+                    idx1, code1 = index_to_code[token1_index]
+                    temp.append((code1, idx1, 'computedFrom', [index_to_code[x][1] for x in right_tokens_index],
+                                 [index_to_code[x][0] for x in right_tokens_index]))
+                    states[code1] = [idx1]
+                DFG += temp
+            if root_node.children[-1].type == "block":
+                temp, states = DFG_python(root_node.children[-1], index_to_code, states)
+                DFG += temp
+        dic = {}
+        for x in DFG:
+            if (x[0], x[1], x[2]) not in dic:
+                dic[(x[0], x[1], x[2])] = [x[3], x[4]]
+            else:
+                dic[(x[0], x[1], x[2])][0] = list(set(dic[(x[0], x[1], x[2])][0] + x[3]))
+                dic[(x[0], x[1], x[2])][1] = sorted(list(set(dic[(x[0], x[1], x[2])][1] + x[4])))
+        DFG = [(x[0], x[1], x[2], y[0], y[1]) for x, y in sorted(dic.items(), key=lambda t: t[0][1])]
+        return sorted(DFG, key=lambda x: x[1]), states
+    elif root_node.type in while_statement:
+        DFG = []
+        for i in range(2):
+            for child in root_node.children:
+                temp, states = DFG_python(child, index_to_code, states)
+                DFG += temp
+        dic = {}
+        for x in DFG:
+            if (x[0], x[1], x[2]) not in dic:
+                dic[(x[0], x[1], x[2])] = [x[3], x[4]]
+            else:
+                dic[(x[0], x[1], x[2])][0] = list(set(dic[(x[0], x[1], x[2])][0] + x[3]))
+                dic[(x[0], x[1], x[2])][1] = sorted(list(set(dic[(x[0], x[1], x[2])][1] + x[4])))
+        DFG = [(x[0], x[1], x[2], y[0], y[1]) for x, y in sorted(dic.items(), key=lambda t: t[0][1])]
+        return sorted(DFG, key=lambda x: x[1]), states
+    else:
+        DFG = []
+        for child in root_node.children:
+            if child.type in do_first_statement:
+                temp, states = DFG_python(child, index_to_code, states)
+                DFG += temp
+        for child in root_node.children:
+            if child.type not in do_first_statement:
+                temp, states = DFG_python(child, index_to_code, states)
+                DFG += temp
+
+        return sorted(DFG,key=lambda x:x[1]),states
+
+def tree_to_variable_index(root_node, index_to_code):
+    if (len(root_node.children) == 0 or root_node.type == 'string') and root_node.type != 'comment':
+        index = (root_node.start_point, root_node.end_point)
+        _, code = index_to_code[index]
+        if root_node.type != code:
+            return [(root_node.start_point, root_node.end_point)]
+        else:
+            return []
+    else:
+        code_tokens = []
+        for child in root_node.children:
+            code_tokens += tree_to_variable_index(child, index_to_code)
+        return code_tokens
+
+
+def index_to_code_token(index, code):
+    start_point = index[0]
+    end_point = index[1]
+    if start_point[0] == end_point[0]:
+        s = code[start_point[0]][start_point[1]:end_point[1]]
+    else:
+        s = ""
+        s += code[start_point[0]][start_point[1]:]
+        for i in range(start_point[0] + 1, end_point[0]):
+            s += code[i]
+        s += code[end_point[0]][:end_point[1]]
+    return s
+
+def tree_to_token_index(root_node):
+    if (len(root_node.children) == 0 or root_node.type == 'string') and root_node.type != 'comment':
+        return [(root_node.start_point, root_node.end_point)]
+    else:
+        code_tokens = []
+        for child in root_node.children:
+            code_tokens += tree_to_token_index(child)
+        return code_tokens
+
+def extract_dataflow(code, parser,lang):
+    #obtain dataflow
+    if lang=="php":
+        code="<?php"+code+"?>"
+    try:
+        tree = parser[0].parse(bytes(code,'utf8'))
+        root_node = tree.root_node
+        tokens_index=tree_to_token_index(root_node)
+        code=code.split('\n')
+        code_tokens=[index_to_code_token(x,code) for x in tokens_index]
+        index_to_code={}
+        for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
+            index_to_code[index]=(idx,code)
+        try:
+            DFG,_=parser[1](root_node,index_to_code,{})
+        except:
+            DFG=[]
+        DFG=sorted(DFG,key=lambda x:x[1])
+        indexs=set()
+        for d in DFG:
+            if len(d[-1])!=0:
+                indexs.add(d[1])
+            for x in d[-1]:
+                indexs.add(x)
+        new_DFG=[]
+        for d in DFG:
+            if d[1] in indexs:
+                new_DFG.append(d)
+        dfg=new_DFG
+    except:
+        dfg=[]
+    return code_tokens, dfg
